6ef7db41efe2be7070bd2fbe66e988a1216752f9
akun <aliceliyukun@outlook.com>
2020-02-08 00:49:46 -0600

    milestone 1

--- app/src/main/java/com/example/lab2/MainActivity.java
+++ app/src/main/java/com/example/lab2/MainActivity.java
@@ -3,11 +3,11 @@ package com.example.lab2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.util.Log;
+
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
 
     public void clickFunction(View view) {
        // Log.i( "Info", "Button Pressed");
-        EditText myTextField = (EditText) findViewById(R.id.textView);
+        EditText myTextField = (EditText) findViewById(R.id.myTextField);
 
        // Toast.makeText(MainActivity.this, myTextField.getText().toString(), Toast.LENGTH_LONG).show();
        String str = myTextField.getText().toString();
